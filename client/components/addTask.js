31928a92f69234701654e8b5751161ab11883ea2
danremel <remel.dan@gmail.com>
2017-12-09 02:09:43 -0500

    Built out addTask.js and .html

--- client/components/addTask.js
+++ client/components/addTask.js
@@ -13,26 +13,30 @@ angular.module('todo-app')
         }
       });
     }
-  $ctrl.addTask = (task) => {
-    // Checks if the task is blank
-    if (!task) {
-      return alert('Please enter a task');
+    $ctrl.addTask = (task) => {
+      // Checks if the task is blank
+      if (!task) {
+        return alert('Please enter a task');
+      }
+      checkList($ctrl.tasks, task);
+      // Returns a true dup if the input already exists
+      if (dup) {
+        dup = false;
+        return alert('That task already exists');
+      }
+      // Sets our task to an object that can be posted
+      task = {
+        task: task,
+        editing: false // Used to access edit
+      };
+      $http.post('/api/tasks', task)
+      .then((res) => {
+        $ctrl.tasks.push(res.data); // Adds a task to the task list
+        $ctrl.task = ''; // Resets the input field upon submission
+      })
     }
-    checkList($ctrl.tasks, task);
-    // Returns a true dup if the input already exists
-    if (dup) {
-      dup = false;
-      return alert('That task already exists');
-    }
-    // Sets our task to an object that can be posted
-    task = {
-      task: task,
-      editing: false // Used to access edit
-    };
-    $http.post('/api/tasks', task)
-    .then((res) => {
-      $ctrl.tasks.push(res.data); // Adds a task to the task list
-      $ctrl.task = ''; // Resets the input field upon submission
-    })
+  },
+  bindings: {
+    tasks: '<'
   }
 });
