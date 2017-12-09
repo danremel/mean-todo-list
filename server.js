fa06be30875d5b1171b78d290aeca32904dfcf2a
danremel <remel.dan@gmail.com>
2017-12-09 00:43:59 -0500

    Set up view engine and basic routes in server.js

--- server.js
+++ server.js
@@ -6,5 +6,23 @@ const index = require('./routes/index');
 const tasks = require('./routes/tasks');
 
 const app = express();
-
+// PORT
 let port = process.env.PORT || 3000;
+
+// View Engine (ejs)
+app.set('viewws', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // Sets the engine in use
+app.engine('html', require('ejs').renderFile); // Renders files with html extension
+
+// Static Folder
+app.use(express.static(__dirname, 'client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index); // Home page route
+app.use('/api', tasks); // API call routes
+
+// Start the server
+app.listen(port, function() {
+  console.log('App connected to port: ', port);
+});
